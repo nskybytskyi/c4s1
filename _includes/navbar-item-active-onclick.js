4a0f13b4d2ed4cba16f44128e35393ea8e646e5f
Sky-Nik <n.skybytskyi@gmail.com>
2019-10-21 09:10:21 +0300

    try more bootstrap-like header ?

--- _includes/navbar-item-active-onclick.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('a[href*="#"]').on('click', function(e) {
-        // e.preventDefault()
-        oldObjChild = $('.active > a'); // gets active nav-item child nav-link
-        oldObj = $('.active'); // gets the active nav-item
-        oldObj.removeClass('active'); // remove active from old nav-item
-        $(this).parent().addClass('active'); // set the active class on the nav-item that called the function
-    });
-});
